8fef7193f621732af6576900b2f4060c9130670a
Legal-Dan <clemies2000@hotmail.com>
2022-05-18 11:25:27 +0100

    Remove highest stat from rolled characteristics

--- src/main/kotlin/CharacterApp.kt
+++ src/main/kotlin/CharacterApp.kt
@@ -1,5 +1,3 @@
-
-
 class CharacterApp(reader: Reader) {
     private val reader = reader
 
@@ -8,9 +6,17 @@ class CharacterApp(reader: Reader) {
         val name = reader.getNextLine()
         println("Standard characteristics or roll:")
         val statsGeneration = reader.getNextLine()
-        println("Highest value (or leave blank for random):")
-        val input = reader.getNextLine()
-        val highestValue = if (input != "") input else "none"
+        val highestValue: String
+        if (statsGeneration == "standard") {
+            println("Highest value (or leave blank for random):")
+            val input = reader.getNextLine()
+            if (input != ""){
+                highestValue = input
+            } else {
+                highestValue = "none"}
+        } else {
+            highestValue = "none"
+        }
         var character = ""
         for (characteristic in PlayableCharacter(statsGeneration, highestValue).characteristics.assignedCharacteristic){
             character += ", " + characteristic.name + ": " + characteristic.value
---
+++ src/test/kotlin/PlayableCharacterTest.kt
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test
+
+class PlayableCharacterTest{
+
+    @Test
+    fun `can generate a set of standard stats`() {
+        val statsGeneration = "standard"
+        val highestValue = "none"
+        var totalValue = 0
+        for (charac in PlayableCharacter(statsGeneration, highestValue).characteristics.assignedCharacteristic) {
+            totalValue += charac.value
+        }
+        assert(totalValue.equals(70+60+60+50+50+50+40+40))
+    }
+
+    @Test
+    fun `can identify a characteristic to be the highest value in standard generation`() {
+        val statsGeneration = "standard"
+        val highestValue = "dexterity"
+        val returnValue =
+            PlayableCharacter(statsGeneration, highestValue).characteristics.assignedCharacteristic[0].value
+        assert(returnValue.equals(70))
+    }
+
+    @Test
+    fun `can generate a set of random stats`() {
+        val statsGeneration = "roll"
+        val highestValue = "none"
+        var totalValue = 0
+        for (charac in PlayableCharacter(statsGeneration, highestValue).characteristics.assignedCharacteristic) {
+            totalValue += charac.value
+        }
+        assert(!totalValue.equals(70+60+60+50+50+50+40+40))
+    }
+}
