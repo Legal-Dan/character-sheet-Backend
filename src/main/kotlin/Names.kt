4b8a4596cdbaa83b1bbade5bb45f2cac0116c183
Legal-Dan <clemies2000@hotmail.com>
2022-07-13 10:52:06 +0100

    Add support for anglo saxon names

--- src/main/kotlin/Names.kt
+++ src/main/kotlin/Names.kt
@@ -7,12 +7,31 @@ class Names(region:String, male:List<String>, female:List<String>, surnames:List
     val languages = languages
 
     fun randomName(gender:String):String{
-        if (gender == "male"){
-            return "${male.random()} ${surnames.random()}"
-        } else if (gender == "female"){
-            return "${female.random()} ${surnames.random()}"
-        } else {
-            return listOf("${male.random()} ${surnames.random()}", "${female.random()} ${surnames.random()}").random()
+        return when(region) {
+        "Kent" -> angloSaxonName(gender)
+        "East Anglia" -> angloSaxonName(gender)
+        "Essex" -> angloSaxonName(gender)
+        "Mercia" -> angloSaxonName(gender)
+        "Northumbria" -> angloSaxonName(gender)
+        "Sussex" -> angloSaxonName(gender)
+        "Wessex" -> angloSaxonName(gender)
+        else -> forenameFirst(gender)
+        }
+    }
+
+    private fun forenameFirst(gender: String): String {
+        return when (gender){
+            "male" -> "${male.random()} ${surnames.random()}"
+            "female" -> "${female.random()} ${surnames.random()}"
+            else -> listOf("${male.random()} ${surnames.random()}", "${female.random()} ${surnames.random()}").random()
+        }
+    }
+
+    private fun angloSaxonName(gender: String): String {
+        return when (gender){
+            "male" -> "${surnames.random()}${male.random()}"
+            "female" -> "${surnames.random()}${female.random()}"
+            else -> listOf("${surnames.random()}${male.random()}", "${surnames.random()}${female.random()}").random()
         }
     }
 }
