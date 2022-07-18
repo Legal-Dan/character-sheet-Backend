0e295db43fe102f17b75c5d01fd71c59a500695f
Legal-Dan <clemies2000@hotmail.com>
2022-07-18 20:48:56 +0100

    Add skills creation tests

---
+++ src/test/kotlin/AssignedSkillsTest.kt
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test
+
+class AssignedSkillsTest{
+
+
+    @Test
+    fun `updates dodge to match half dexterity`() {
+        assert(TestFunctions().assignedSkills("classic").assignedCharacterSkills["dodge"]!!.initialValue == 50)
+    }
+
+    @Test
+    fun `updates language display name to match country's language`() {
+        assert(TestFunctions().assignedSkills("classic").assignedCharacterSkills["languageOwn"]!!.displayName == "Language: MyLanguage")
+    }
+
+    @Test
+    fun `updates language value to match education`() {
+        assert(TestFunctions().assignedSkills("classic").assignedCharacterSkills["languageOwn"]!!.initialValue >= 500)
+    }
+
+    @Test
+    fun `total points equal to standard array`() {
+        var total = 0
+        for (skill in TestFunctions().assignedSkills("classic").assignedCharacterSkills) {
+            total += skill.value.initialValue
+        }
+        assert(total == 70 + 60 + 60 + 50 + 50 + 50 + 40 + 40 + 40 + (75 * 2) + 500 + (100 / 2))
+    }
+
+
+}
---
+++ src/test/kotlin/TestFunctions.kt
@@ -0,0 +1,32 @@
+class TestFunctions {
+    fun characterSkills(era: String): MutableMap<String, Skills> {
+        return mutableMapOf(
+        "dodge" to Skills("Dodge", listOf(era), 0, "Common", null),
+        "languageOwn" to Skills("languageown", listOf(era), 0, "Common", null),
+        "creditRating" to Skills("Credit Rating", listOf(era), 0, "Common", null),
+        "sciences" to Skills("Sciences", listOf(era), 0, "Common", mutableMapOf("Agriculture" to 0)),
+        "driveAuto" to Skills("Drive Auto", listOf(era), 0, "Common", null),
+        "mechRepair" to Skills("Mechanical Repair", listOf(era), 0, "Common", null),
+        "naturalWorld" to Skills("Natural World", listOf(era), 0, "Common", null),
+        "operateHeavyMachinery" to Skills("Operate Heavy Machinery", listOf(era), 0, "Common", null),
+        "track" to Skills("Track", listOf(era), 0, "Common", null),
+        "charm" to Skills("Charm", listOf(era), 0, "Common", null),
+        "fastTalk" to Skills("Fast Talk", listOf(era), 0, "Common", null),
+        "intimidate" to Skills("Intimidate", listOf(era), 0, "Common", null),
+        "persuade" to Skills("Persuade", listOf(era), 0, "Common", null)
+    )}
+
+    fun assignedSkills(era:String): AssignedSkills {
+        return AssignedSkills(
+            characterSkills(era),
+            importOccupations("Farmer", "classic"),
+            "classic",
+            mutableMapOf(
+                "dexterity" to Characteristic("dexterity", 100),
+                "education" to Characteristic("education", 500),
+                "intelligence" to Characteristic("intelligence", 75)
+            ),
+            Country("CountryName", listOf("MaleName"), listOf("FemaleName"), listOf("Surname"), listOf("MyLanguage"))
+        )
+    }
+}
